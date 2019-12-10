d340ede2665704efdbb17c30d5174c9c2c31a735
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-12-10 22:09:41 +0100

    files

--- travel/01/js/main.js
+++ travel/01/js/main.js
@@ -5,3 +5,17 @@ el.onclick = function() {
 	document.querySelector('body').classList.toggle('open');
 	document.querySelector('.burger').classList.toggle('open');
 }
+
+function onReady(callback) {
+	var intervalId = window.setInterval(function() {
+		if (document.getElementsByTagName('body')[0] !== undefined) {
+			window.clearInterval(intervalId);
+			callback.call(this);
+		}
+	}, 3000);
+}
+
+onReady(function() {
+	document.querySelector('body').classList.remove('loading');
+	document.querySelector('body').classList.add('loaded');
+});

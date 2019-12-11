b9fd3c93b38e7e987196199b39607f237d26694e
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-12-11 16:49:09 +0100

    files

--- admin/01/js/main.js
+++ admin/01/js/main.js
@@ -1,7 +1,17 @@
 document.querySelector('body').classList.remove('preload');
 
-var el = document.querySelector('.nav-toggle');
-el.onclick = function() {
-	document.querySelector('body').classList.toggle('open');
-	document.querySelector('.burger').classList.toggle('open');
-}
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+	responsive: true,
+    type: 'line',
+    data: {
+    	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    	datasets: [{
+    		label: 'My First dataset',
+    		backgroundColor: 'rgb(255, 99, 132)',
+    		borderColor: 'rgb(255, 99, 132)',
+    		data: [0, 10, 5, 2, 20, 30, 45]
+    	}]
+    },
+    options: {}
+});

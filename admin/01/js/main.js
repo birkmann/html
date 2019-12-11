3bc8b98284f1c9200635311533d568e70472b9ca
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-12-11 20:28:19 +0100

    files

--- admin/01/js/main.js
+++ admin/01/js/main.js
@@ -1,49 +1,79 @@
 document.querySelector('body').classList.remove('preload');
 
-var ctx = document.getElementById('chartActivity').getContext('2d');
-var chart = new Chart(ctx, {
-	responsive: true,
-	type: 'line',
+new Chart(document.getElementById("bar-chart"), {
+	type: 'bar',
 	data: {
-		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-		datasets: [{
-			label: 'Activity',
-			backgroundColor: 'rgb(255, 99, 132)',
-			borderColor: 'rgb(255, 99, 132)',
-			data: [0, 10, 5, 2, 20, 30, 45]
-		}]
+		labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+		datasets: [
+		{
+			label: "Population (millions)",
+			backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+			data: [2478,5267,734,784,433]
+		}
+		]
 	},
-	options: {}
+	options: {
+		legend: { display: false },
+		title: {
+			
+		}
+	}
 });
 
-var ctx = document.getElementById('chartInbound').getContext('2d');
-var chart = new Chart(ctx, {
-	responsive: true,
+new Chart(document.getElementById("line-chart"), {
 	type: 'line',
 	data: {
-		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-		datasets: [{
-			label: 'Activity',
-			backgroundColor: 'rgb(255, 99, 132)',
-			borderColor: 'rgb(255, 99, 132)',
-			data: [0, 10, 5, 2, 20, 30, 45]
-		}]
+		labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+		datasets: [{ 
+			data: [86,114,106,106,107,111,133,221,783,2478],
+			label: "Africa",
+			borderColor: "#3e95cd",
+			fill: false
+		}, { 
+			data: [282,350,411,502,635,809,947,1402,3700,5267],
+			label: "Asia",
+			borderColor: "#8e5ea2",
+			fill: false
+		}, { 
+			data: [168,170,178,190,203,276,408,547,675,734],
+			label: "Europe",
+			borderColor: "#3cba9f",
+			fill: false
+		}, { 
+			data: [40,20,10,16,24,38,74,167,508,784],
+			label: "Latin America",
+			borderColor: "#e8c3b9",
+			fill: false
+		}, { 
+			data: [6,3,2,2,7,26,82,172,312,433],
+			label: "North America",
+			borderColor: "#c45850",
+			fill: false
+		}
+		]
 	},
-	options: {}
+	options: {
+		title: {
+			display: true,
+			text: 'World population per region (in millions)'
+		}
+	}
 });
 
-var ctx = document.getElementById('chartOutbound').getContext('2d');
-var chart = new Chart(ctx, {
-	responsive: true,
-	type: 'line',
+new Chart(document.getElementById("pie-chart"), {
+	type: 'pie',
 	data: {
-		labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+		labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
 		datasets: [{
-			label: 'Activity',
-			backgroundColor: 'rgb(255, 99, 132)',
-			borderColor: 'rgb(255, 99, 132)',
-			data: [0, 10, 5, 2, 20, 30, 45]
+			label: "Population (millions)",
+			backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+			data: [2478,5267,734,784,433]
 		}]
 	},
-	options: {}
+	options: {
+		title: {
+			display: true,
+			text: 'Predicted world population (millions) in 2050'
+		}
+	}
 });

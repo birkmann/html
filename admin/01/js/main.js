72ee9d034b5945a0fcc07626cf3c29f8f2d87918
Rafael Birkmann <mail@rafaelbirkmann.de>
2019-12-11 16:54:20 +0100

    files

--- admin/01/js/main.js
+++ admin/01/js/main.js
@@ -1,13 +1,45 @@
 document.querySelector('body').classList.remove('preload');
 
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('chartActivity').getContext('2d');
 var chart = new Chart(ctx, {
 	responsive: true,
     type: 'line',
     data: {
     	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     	datasets: [{
-    		label: 'My First dataset',
+    		label: 'Activity',
+    		backgroundColor: 'rgb(255, 99, 132)',
+    		borderColor: 'rgb(255, 99, 132)',
+    		data: [0, 10, 5, 2, 20, 30, 45]
+    	}]
+    },
+    options: {}
+});
+
+var ctx = document.getElementById('chartInbound').getContext('2d');
+var chart = new Chart(ctx, {
+	responsive: true,
+    type: 'line',
+    data: {
+    	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    	datasets: [{
+    		label: 'Activity',
+    		backgroundColor: 'rgb(255, 99, 132)',
+    		borderColor: 'rgb(255, 99, 132)',
+    		data: [0, 10, 5, 2, 20, 30, 45]
+    	}]
+    },
+    options: {}
+});
+
+var ctx = document.getElementById('chartOutbound').getContext('2d');
+var chart = new Chart(ctx, {
+	responsive: true,
+    type: 'line',
+    data: {
+    	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    	datasets: [{
+    		label: 'Activity',
     		backgroundColor: 'rgb(255, 99, 132)',
     		borderColor: 'rgb(255, 99, 132)',
     		data: [0, 10, 5, 2, 20, 30, 45]
